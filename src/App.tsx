7a1ef36de50d4716063985d06d637663994828bd
ErezKon <erezkon@gmail.com>
2026-08-06 18:03:37 +0300

    [pacman4]-[CHORE]-chore: final cleanup for pacman4/chore/scaffold

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>Pac-Man</h1>
+      <p>Welcome to Pac-Man game!</p>
+    </div>
+  );
+};
